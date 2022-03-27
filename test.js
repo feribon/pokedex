9750f11a4effd7476be79c9ddec3f2b495e2edb9
Felipe Grassi Ribon <60076271+feribon@users.noreply.github.com>
2022-03-27 15:40:19 -0300

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const retornaNome = () => {
-    const url2 = `https://pokeapi.co/api/v2/pokemon/1`
-    fetch(url2)
-    .then(response => response.json())
-    .then(pokemon =>{
-        console.log(pokemon.name)
-    })
-}
-retornaNome()
